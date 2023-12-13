8773b38f0e81676ff6c833ffa3d3a586d87a000b
jnevin7 <144211993+jnevin7@users.noreply.github.com>
2023-12-13 21:09:29 +0200

    Add files via upload

---
+++ Chapter2/chapter2.js
@@ -0,0 +1,117 @@
+"use strict";
+
+// let nr1 = 12;
+// nr1 = 8;
+
+// const someConstant = 3;
+// someConstant = 5;
+
+// let language = "Javascript";
+// let message = `Let's learn ${language}`;
+// alert(message); 
+
+// let intNr = 1;
+// let bigNr = 90071992547409920n;
+// let bigNr2 = bigNr + intNr;
+
+// console.log(bigNr2)
+
+// let str1 = "JavaScript is fun!";
+// let str2 = "JavaScript is fun!";
+// console.log("These two strings are the same:", str1 === str2);
+
+// let sym1 = Symbol("JavaScript is fun!");
+// let sym2 = Symbol("JavaScript is fun!");
+// console.log("These two symbols are the same:", sym1 === sym2);
+
+// let variable = 1;
+// let variable = "Hello!"
+// let variable = false;
+// let variable = [5];
+// alert("Type of variable is " + typeof variable)
+
+// let nr1 = String(2);
+// let nr2 = "3";
+// console.log(nr1 + nr2)
+
+// let nrToStr = 6;
+// nrToStr = String(nrToStr);
+// console.log(nrToStr, typeof nrToStr);
+
+// let strToNr = "12";
+// strToNr = Number(strToNr);
+// console.log(strToNr, typeof strToNr);
+
+// let strToBool = "any string will return true";
+// strToBool = Boolean(strToBool);
+// console.log(strToBool, typeof strToBool);
+
+// let nullToNr = null;
+// nullToNr = Number(nullToNr)
+// console.log("null", nullToNr, typeof nullToNr);
+
+// let strToNr = "";
+// strToNr = Number(strToNr);
+// console.log("empty string", strToNr, typeof strToNr);
+
+// let strToNr2 = "hello";
+// strToNr2 = Number(strToNr2);
+// console.log(strToNr2, typeof strToNr2);
+
+// let strToBool2 = "false";
+// strToBool2 = Boolean(strToBool2);
+// console.log(strToBool2, typeof strToBool2);
+
+// let strToBool = "";
+// strToBool = Boolean(strToBool);
+// console.log(strToBool, typeof strToBool);
+
+// let nr1 = 2;
+// let nr2 = "2";
+// console.log(nr1 + Number(nr2));
+
+// let str1 = 'Laurence';  //string
+// let str2 = "Svekis";  //string
+// let val1 = undefined; //undefined
+// let val2 = null; //null x object
+// let myNum = 1000; //number
+// console.log(typeof str1, typeof str2, typeof val1, typeof val2, typeof myNum)
+
+// let nr1 = 2;
+// let nr2 = 3;
+// let result1 = nr1 ** nr2;
+// console.log(result1);
+
+// let nr1 = 4;
+// console.log(nr1++);
+// console.log(nr1);
+
+// let nr2 = 4;
+// nr2--;
+// console.log(nr2);
+
+// let nr = 2;
+// console.log(nr++);
+// console.log(nr);
+
+// let a = Number(prompt("Provide adjacent length:"))
+// let b = Number(prompt("Provide opposite length:"))
+// alert("The length of the hypotenuse is " + a**2 + b**2)
+
+//Chapter Project
+
+// let distanceMiles = Number(prompt("Enter number of miles:"));
+// let distanceKm = distanceMiles*1.60934;
+// alert("The distance of " + distanceKm + "kms is equal to " + distanceMiles + " miles");
+
+// let inch = 60
+// let cm = inch*2.54
+
+// let kilo = 60
+// let pounds = kilo*2.2046
+
+// let BMI = kilo/((cm/100)*(cm/100))
+
+// console.log(cm, pounds, BMI)
+
+
